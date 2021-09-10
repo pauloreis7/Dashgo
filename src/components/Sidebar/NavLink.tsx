3ddb408763b2790bc20ac87222b0e651435c8a7b
Paulo Reis <paulosilvadosreis2057@gmail.com>
2021-09-10 12:49:26 -0300

    Apply active link feature to sidebar and add navigation to the app

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,17 +1,22 @@
-import { Link, Text, Icon, LinkProps as ChakraLinkProps } from "@chakra-ui/react";
+import { Link as ChakraLink, Text, Icon, LinkProps as ChakraLinkProps } from "@chakra-ui/react";
 import { ElementType, ReactNode } from "react";
+
+import { ActiveLink } from "../ActiveLink";
 
 interface NavLinkProps extends ChakraLinkProps {
   icon: ElementType;
   children: ReactNode;
+  href: string;
 }
 
-export function NavLink({ icon, children, ...rest }: NavLinkProps) {
+export function NavLink({ icon, children, href, ...rest }: NavLinkProps) {
   return (
-    <Link display="flex" align="center" {...rest}>
-      <Icon as={icon} fontSize="20" />
+    <ActiveLink href={href} passHref>
+      <ChakraLink display="flex" align="center" {...rest}>
+        <Icon as={icon} fontSize="20" />
 
-      <Text ml="4" fontWeight="medium">{children}</Text>
-    </Link>
+        <Text ml="4" fontWeight="medium">{children}</Text>
+      </ChakraLink>
+    </ActiveLink>
   )
 }
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -14,6 +14,7 @@ import {
   Checkbox,
   useBreakpointValue
 } from '@chakra-ui/react'
+import Link from 'next/link'
 import { RiAddLine, RiPencilLine } from 'react-icons/ri'
 
 import { Header } from '../../components/Header'
@@ -38,16 +39,18 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">Usuários</Heading>
 
-            <Button
-              as="a"
-              size="sm"
-              fontSize="sm"
-              colorScheme="pink"
-              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
-              cursor="pointer"
-            >
-              Criar novo
-            </Button>
+            <Link href="/users/create" passHref>
+              <Button
+                as="a"
+                size="sm"
+                fontSize="sm"
+                colorScheme="pink"
+                leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+                cursor="pointer"
+              >
+                Criar novo
+              </Button>
+            </Link>
           </Flex>
 
             <Table overflowX="scroll" colorScheme="whiteAlpha">
