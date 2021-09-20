ecaa27ad470f006b329b9fbffcf8f08a12d40710
Paulo Reis <paulosilvadosreis2057@gmail.com>
2021-09-20 09:45:01 -0300

    Add useUsers hook for users list

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -17,33 +17,15 @@ import {
 } from '@chakra-ui/react'
 import Link from 'next/link'
 import { RiAddLine, RiPencilLine } from 'react-icons/ri'
-import { useQuery } from 'react-query'
 
-import { api } from '../../services/api'
+import { useUsers } from '../../services/hooks/useUsers'
 
 import { Header } from '../../components/Header'
 import { Sidebar } from '../../components/Sidebar'
 import { Pagination } from '../../components/Pagination'
 
 export default function UserList() {
-  const { data, isLoading, isFetching, error } = useQuery('users', async () => {
-    const { data } = await api.get('users')
-
-    const users = data.users.map(user => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric'
-        }),
-      }
-    })
-
-    return users
-  })
+  const { data, isLoading, isFetching, error } = useUsers()
 
   const isMediumVersion = useBreakpointValue({
     base: false,
