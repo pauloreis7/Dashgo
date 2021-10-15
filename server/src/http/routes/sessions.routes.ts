dbc1db8a9e0b6e74a629db5e41047e0d1361299f
Paulo Reis <paulosilvadosreis2057@gmail.com>
2021-10-15 12:02:34 -0300

    Add celebrate middleware to routes

--- server/src/http/routes/sessions.routes.ts
+++ server/src/http/routes/sessions.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { celebrate, Segments, Joi } from 'celebrate'
 
 import { SessionsController } from '../../controllers/SessionsController'
 
@@ -7,14 +8,35 @@ import { addUserInformationToRequest } from '../middlewares/addUserInformationTo
 const sessionsRouter = Router()
 const sessionsController = new SessionsController()
 
-sessionsRouter.post('/', sessionsController.create);
+sessionsRouter.post('/',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+      password: Joi.string().required()
+    },
+  }),
+  sessionsController.create
+);
 
 sessionsRouter.post(
   '/refresh', 
+  celebrate({
+    [Segments.BODY]: {
+      refresh_token: Joi.string().uuid().required(),
+    },
+  }),
   addUserInformationToRequest,
   sessionsController.refresh
 );
 
-sessionsRouter.delete('/logout', sessionsController.logout)
+sessionsRouter.delete(
+  '/logout',
+  celebrate({
+    [Segments.BODY]: {
+      refresh_token: Joi.string().uuid().required(),
+    },
+  }),
+  sessionsController.logout
+ )
 
 export default sessionsRouter
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import express from 'express';
 import cors from 'cors';
+import { errors } from 'celebrate'
 
 import routes from './http/routes'
 
@@ -11,5 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(cors())
 app.use(routes)
+
+app.use(errors())
 
 app.listen(3333);
