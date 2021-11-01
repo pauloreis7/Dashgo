9cc10b868bc561c14cc8f2f33d3857724f0002fd
Paulo Reis <paulosilvadosreis2057@gmail.com>
2021-10-31 21:24:05 -0300

    Connect create leads frontend feature with api

--- web/src/pages/leads/create.tsx
+++ web/src/pages/leads/create.tsx
@@ -1,4 +1,4 @@
-import { Flex, Box, Heading, Divider, Stack, SimpleGrid, HStack, Button } from '@chakra-ui/react'
+import { Flex, Box, Heading, Divider, Stack, HStack, Button, useToast } from '@chakra-ui/react'
 import Link from 'next/link'
 import * as yup from 'yup'
 import { useForm, SubmitHandler } from 'react-hook-form'
@@ -12,48 +12,58 @@ import { Input } from '../../components/Form/Input'
 import { Header } from '../../components/Header'
 import { Sidebar } from '../../components/Sidebar'
 
-type CreateUserFormData = {
+type CreateLeadFormData = {
   name: string;
   email: string;
-  password: string;
-  password_confirmation: string;
 }
 
-const createUserFormSchema = yup.object().shape({
+const createLeadFormSchema = yup.object().shape({
   name: yup.string().required('Nome obrigatório'),
   email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
-  password: yup.string().required('Senha obrigatória').min(6, 'Mínimo de 6 caracteres'),
-  password_confirmation: yup.string().oneOf([
-    null, yup.ref('password')
-  ], 'As senhas precisam ser iguais'),
 })
 
-export default function CreateUser() {
+export default function CreateLead() {
   const router = useRouter()
 
-  const createUser = useMutation(async (user: CreateUserFormData) => {
-    const response = await api.post('users', {
-      user: {
-        ...user,
-        created_at: new Date(),
-      }
-    })
+  const toast = useToast()
 
-    return response.data.user
+  const createLead = useMutation(async (lead: CreateLeadFormData) => {
+    const response = await api.post('/leads/create', lead)
+
+    return response.data.lead
   }, {
-    onSuccess: () => {
-      queryClient.invalidateQueries('users')
+    onSuccess: (_, lead) => {
+      queryClient.invalidateQueries('leads')
+
+      toast({
+        title: "Lead criado.",
+        description: `Lead ${lead.name} foi criado com sucesso.`,
+        status: "success",
+        duration: 6000,
+        isClosable: true,
+        position: "top-right",
+      })
+    },
+    onError: (err) => {
+      toast({
+        title: "Erro ao criar lead.",
+        description: err,
+        status: "error",
+        duration: 6000,
+        isClosable: true,
+        position: "top-right",
+      })
     }
   })
 
   const { register, handleSubmit, formState } = useForm({
-    resolver: yupResolver(createUserFormSchema)
+    resolver: yupResolver(createLeadFormSchema)
   })
 
   const { errors, isSubmitting } = formState
 
-  const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
-    await createUser.mutateAsync(values)
+  const handleCreateLead: SubmitHandler<CreateLeadFormData> = async (values) => {
+    await createLead.mutateAsync(values)
 
     router.push('/leads')
   }
@@ -71,45 +81,26 @@ export default function CreateUser() {
           borderRadius={8}
           bg="gray.800"
           p={["6", "8"]}
-          onSubmit={handleSubmit(handleCreateUser)}
+          onSubmit={handleSubmit(handleCreateLead)}
         >
           <Heading size="lg" fontWeight="normal">Criar usuário</Heading>
 
           <Divider my="6" borderColor="gray.700" />
         
           <Stack spacing="8">
-            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
-              <Input
-                name="name"
-                label="Nome completo"
-                error={errors.name}
-                {...register('name')}
-              />
-              <Input
-                name="email"
-                type="email"
-                label="E-mail"
-                error={errors.email}
-                {...register('email')}
-              />
-            </SimpleGrid>
-
-            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
-              <Input
-                name="password"
-                type="password"
-                label="Senha"
-                error={errors.password}
-                {...register('password')}
-              />
-              <Input
-                name="password_confirmation"
-                type="password"
-                label="Confirmação da senha"
-                error={errors.password_confirmation}
-                {...register('password_confirmation')}
-              />
-            </SimpleGrid>
+            <Input
+              name="name"
+              label="Nome completo"
+              error={errors.name}
+              {...register('name')}
+            />
+            <Input
+              name="email"
+              type="email"
+              label="E-mail"
+              error={errors.email}
+              {...register('email')}
+            />
           </Stack>
 
           <Flex mt="8" justify="flex-end">
@@ -123,7 +114,7 @@ export default function CreateUser() {
                 colorScheme="pink"
                 isLoading={isSubmitting} 
               >
-                Salvar
+                Criar
               </Button>
             </HStack>
           </Flex>
