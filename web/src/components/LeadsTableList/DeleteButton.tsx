52d9129289abcbf4a46fd093bb858f3eef5734a3
Paulo Reis <paulosilvadosreis2057@gmail.com>
2021-10-31 21:25:54 -0300

    Connect delete lead frontend feature with api

---
+++ web/src/components/LeadsTableList/DeleteButton.tsx
@@ -0,0 +1,99 @@
+import { useState } from "react"
+import {
+  IconButton,
+  Button,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  Portal,
+  PopoverArrow,
+  PopoverCloseButton,
+  useToast,
+} from "@chakra-ui/react"
+import { RiDeleteBin6Line } from 'react-icons/ri'
+import { useMutation } from "react-query"
+
+import { api } from "../../services/apiClient"
+import { queryClient } from "../../services/queryClient"
+
+interface DeleteButtonProps {
+  title: string
+  leadId: string
+}
+
+export function DeleteButton({ leadId, title }: DeleteButtonProps) {
+  const toast = useToast()
+  
+  const deleteLead = useMutation(async () => {
+    api.delete('/leads/delete', {
+      params: {
+        leadId
+      }
+    })
+
+    return
+  }, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('leads')
+
+      toast({
+        title: "Lead deletado.",
+        description: "Lead foi deletado com sucesso.",
+        status: "success",
+        duration: 6000,
+        isClosable: true,
+        position: "top-right",
+      })
+    },
+    onError: (err) => {
+      toast({
+        title: "Erro ao deletar o lead.",
+        description: err,
+        status: "error",
+        duration: 6000,
+        isClosable: true,
+        position: "top-right",
+      })
+    }
+  })
+
+  async function handleDeleteLead() {
+    await deleteLead.mutateAsync()
+  }
+
+  return (
+    <Popover>
+      <PopoverTrigger>
+        <IconButton
+          size="sm"
+          aria-label="Editar"
+          _hover={{
+            color: "red.600",
+            transition: "color 0.2s"
+          }}
+          color="gray.300"
+          colorScheme="none"
+          icon={<RiDeleteBin6Line fontSize="20" />}
+        />
+      </PopoverTrigger>
+      <Portal>
+        <PopoverContent bg="gray.800" borderColor="gray.800">
+          <PopoverArrow bgColor="gray.800" />
+          <PopoverHeader borderBottomColor="gray.700">{title}</PopoverHeader>
+          <PopoverCloseButton />
+          <PopoverBody>
+            <Button 
+              colorScheme="red" 
+              onClick={handleDeleteLead}
+              isLoading={useMutation({ mutationKey: 'leads' }).isLoading}
+            >
+              Deletar
+            </Button>
+          </PopoverBody>
+        </PopoverContent>
+      </Portal>
+    </Popover>
+  )
+}
