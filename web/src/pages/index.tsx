afa0ffc781dacbc54f1df3ea14157e0d5d3bc532
Paulo Reis <paulosilvadosreis2057@gmail.com>
2021-11-03 13:52:20 -0300

    Create initial HomePage

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,165 +1,45 @@
-import { 
-  Flex,
-  Text,
-  Button,
-  Stack,
-  Heading,
-  Divider,
-  Icon,
-  useToast,
-  Link as ChakraLink
-} from '@chakra-ui/react'
-import { GetServerSideProps } from 'next'
-import Link from 'next/link'
-import * as yup from 'yup'
-import { useForm, SubmitHandler } from 'react-hook-form'
-import { yupResolver } from '@hookform/resolvers/yup'
+import { useEffect } from "react"
+import { GetStaticProps } from "next"
+import Link from "next/link"
+import Router from 'next/router'
 import { parseCookies } from 'nookies'
-import { RiLoginBoxLine } from 'react-icons/ri'
- 
-import { useAuth } from '../contexts/AuthContext'
-
-import { Input } from '../components/Form/Input'
 
 import { 
-  MotionFlex,
-  MotionStack,
-  flexAnimation, 
-  childrenItemAnimation 
-} from '../components/animations/SignInAnimations'
+  MotionBox, 
+  MotionFlex, 
+  stagger
+} from '../components/animations/GlobalAnimations'
+import { fadeInUpHome } from '../components/animations/HomepageAnimations'
 
-type SignInFormData = {
-  email: string;
-  password: string;
-}
 
-const signInFormSchema = yup.object().shape({
-  email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
-  password: yup.string().required('Senha obrigatória')
-})
+export default function HomePage() {
+  useEffect(() => {
+    const { ['@dashgo.token']: token } = parseCookies()
 
-export default function Home() {
-  const { signIn } = useAuth()
-
-  const toast = useToast()
-
-  const { register, handleSubmit, formState } = useForm({
-    resolver: yupResolver(signInFormSchema)
-  })
-
-  const { errors, isSubmitting } = formState
-
-  const handleSignIn: SubmitHandler<SignInFormData> = async (data) => {
-    try {
-      const { email, password } = data
-
-      await signIn({ email, password })
-      
-      toast({
-        title: "Login realizado.",
-        description: "Login feito com sucesso.",
-        status: "success",
-        duration: 6000,
-        isClosable: true,
-        position: "top-right",
-      })
-    } catch (err) {
-      toast({
-        title: "Erro ao fazer login.",
-        description: err.message,
-        status: "error",
-        duration: 6000,
-        isClosable: true,
-        position: "top-right",
-      })
+    if(token) {
+      Router.push('/dashboard')
     }
-  }
+  }, [])
 
   return (
-    <Flex
-      w="100vw"
-      h="100vh"
-      align="center"
-      justify="center"
-      p="10"
-    >
-      <MotionFlex
-        as="form"
-        width="100%"
-        maxWidth={480}
-        bg="gray.800"
-        p="8"
-        borderRadius={8}
-        flexDir="column"
-        variants={flexAnimation}
-        initial="hidden"
-        animate="visible"
-        onSubmit={handleSubmit(handleSignIn)}
-      >
-        <MotionStack spacing="4" variants={childrenItemAnimation}>
-          <Heading as="h1" size="lg" textAlign="center">Faça Login</Heading>
-
-          <Divider my={["4", "6"]} borderColor="gray.700" />
-
-          <Input 
-            name="email" 
-            type="email" 
-            label="E-mail" 
-            error={errors.email}
-            {...register('email')}
-          />
-
-          <Input
-            name="password"
-            type="password"
-            label="Senha"
-            error={errors.password}
-            {...register('password')}
-          />
-        </MotionStack>
-        
-        <MotionFlex
-          flexDirection="column"
-          variants={childrenItemAnimation}
-        >
-          <Button
-            type="submit"
-            my="6"
-            colorScheme="pink"
-            size="lg"
-            cursor="pointer"
-            rightIcon={<Icon as={RiLoginBoxLine} fontSize="20" />}
-            isLoading={isSubmitting}
-          >
-            Entrar
-          </Button>
-
-          <Text align="center" fontSize="lg">Ainda não tem uma conta? 
-            <Link href="/signup" passHref>
-              <ChakraLink ml="2" color="pink.500" fontWeight="bold">
-                Crie agora
-              </ChakraLink>
-            </Link>
-          </Text>
+    <MotionBox initial='initial' animate='animate' exit={{ opacity: 0 }}>
+      <MotionBox variants={stagger} >
+        <MotionFlex variants={fadeInUpHome} >
+          <h1>HomePage</h1>
         </MotionFlex>
-      </MotionFlex>
-    </Flex>
+      </MotionBox>
+    </MotionBox>
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const { ['@dashgo.token']: token } = parseCookies(ctx)
-  
-  if(token) {
-    return {
-      redirect: {
-        destination: '/dashboard',
-        permanent: false,
-      }
-    }
-  }
-
+export const getStaticProps: GetStaticProps = async () => {
   return {
     props: {},
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }
+
+// sideBar 
+// page transitions
+// scroll
+// image 3d
