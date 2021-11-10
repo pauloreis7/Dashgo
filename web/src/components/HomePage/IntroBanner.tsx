4e1c1203a73b377e9a8c9d84799ffd44ae365d5c
Paulo Reis <paulosilvadosreis2057@gmail.com>
2021-11-09 22:07:21 -0300

    Update IntroBanner animated 3d component layout

--- web/src/components/HomePage/IntroBanner.tsx
+++ web/src/components/HomePage/IntroBanner.tsx
@@ -4,7 +4,8 @@ import { motion, useViewportScroll, useTransform, useMotionValue } from 'framer-
 import { RiUserFollowLine } from 'react-icons/ri'
 
 import { 
-  MotionBox, 
+  MotionBox,
+  MotionFlex,
   stagger
 } from '../animations/GlobalAnimations'
 import { fadeInUpHome } from '../animations/HomepageAnimations'
@@ -14,52 +15,69 @@ export function IntroBanner() {
   const intro3dX = useMotionValue(0)
   const intro3dY = useMotionValue(0)
 
-  const introScrollY = useTransform(scrollYProgress, [0, 0.40], [0, 48])
-  const introTextScrollY = useTransform(scrollYProgress, [0, 0.40], [80, 10])
+  const introScrollY = useTransform(scrollYProgress, [0, 0.40], [0, 200])
+  const introTextScrollY = useTransform(scrollYProgress, [0, 0.40], [160, 30])
 
   const intro3dRotateX = useTransform(intro3dY, [-100, 100], [30, -30])
   const intro3dRotateY = useTransform(intro3dX, [-100, 100], [-30, 30])
 
   return (
-    <MotionBox variants={stagger} style={{ perspective: 2000 }}>
-      <Flex
-        p="6rem 3rem 3rem"
-        justify="space-between"
-        bgColor="gray.800"
+    <MotionFlex 
+      variants={stagger} 
+      style={{ perspective: 2000 }}
+      w="100%"
+      h="100vh"
+      p="6rem 3rem"
+      bgColor="pink.500"
+      position="relative"
+      zIndex={0}
+    >
+      <MotionBox 
+        variants={fadeInUpHome} 
+        maxW="40rem" 
+        flex="1" 
+        position="relative"
+        zIndex={2}
       >
-        <MotionBox variants={fadeInUpHome} flex="1" position="relative">
-          <motion.div
-            style={{ 
-              position: "absolute",
-              top: introTextScrollY,
-            }}
-          >
-            <Text as="h1" mb="5" color="gray.100" fontSize="5xl" fontWeight="bold" lineHeight="3.8rem">
-              Descubra a plataforma para gestão das top empresas
-            </Text>
+        <motion.div
+          style={{ 
+            position: "absolute",
+            top: introTextScrollY,
+          }}
+        >
+          <Text as="h1" mb="5" color="gray.100" fontSize="5xl" fontWeight="bold" lineHeight="3.8rem">
+            Descubra a plataforma para gestão das top empresas
+          </Text>
 
-            <Text maxW="xl" fontSize="lg" mb="6">
-              Dashgo é a agilidade e precisão nos seus dados cumprindo totalmente o papel estratégico e utilizada pelos melhores profissionais
-            </Text>
+          <Text maxW="xl" fontSize="lg" mb="6">
+            Dashgo é a agilidade e precisão nos seus dados cumprindo totalmente o papel estratégico e utilizada pelos melhores profissionais
+          </Text>
 
-            <Link href="/account/signup" passHref>
-              <Button 
-                as="a"
-                size="lg"
-                colorScheme="pink"
-                cursor="pointer"
-                rightIcon={<Icon as={RiUserFollowLine} fontSize="20" />}
-              >
-                Criar conta
-              </Button>
-            </Link>
-          </motion.div>
-        </MotionBox>
+          <Link href="/account/signup" passHref>
+            <Button 
+              as="a"
+              size="lg"
+              colorScheme="pink"
+              cursor="pointer"
+              rightIcon={<Icon as={RiUserFollowLine} fontSize="20" />}
+            >
+              Criar conta
+            </Button>
+          </Link>
+        </motion.div>
+      </MotionBox>
 
+      <motion.div
+        style={{
+          position: "absolute",
+          right: "0",
+          top: introScrollY,
+        }}
+      >
         <MotionBox 
           variants={fadeInUpHome} 
-          w="37.5rem" 
-          h="25.75rem" 
+          w="66rem" 
+          h="100%" 
           position="relative"
           style={{
             cursor: "grab",
@@ -67,26 +85,19 @@ export function IntroBanner() {
             y: intro3dY,
             rotateX: intro3dRotateX,
             rotateY: intro3dRotateY,
-            z: 100
+            z: -1000
           }}
           drag
           dragElastic={0.16}
           dragConstraints={{ top: 0, left: 0, right: 0, bottom: 0 }}
           whileTap={{ cursor: "grabbing" }}
         >
-          <motion.div
-            style={{
-              width: "100%",
-              height: "100%",
-              backgroundImage: "url(/images/intro.gif)",
-              backgroundRepeat: 'no-repeat',
-              backgroundSize: 'cover',
-              position: "absolute",
-              top: introScrollY,
-            }}
-          />
+          <video playsInline autoPlay muted loop>
+            <source src="/videos/introweb.webm" type="video/webm" />
+            <source src="/videos/yt.mp4" type="video/mp4" />
+          </video>
         </MotionBox>
-      </Flex>
-    </MotionBox>
+      </motion.div>
+    </MotionFlex>
   )
 }
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,13 +1,14 @@
 import { useEffect } from "react"
 import { GetStaticProps } from "next"
-import { Flex } from "@chakra-ui/react"
+import { Flex, Box, Text } from "@chakra-ui/react"
 import Router from 'next/router'
+import Image from 'next/image'
 import { parseCookies } from 'nookies'
 
 import { HomeHeader } from '../components/HomePage/HomeHeader'
 import { IntroBanner } from '../components/HomePage/IntroBanner'
 
-import { MotionBox } from '../components/animations/GlobalAnimations'
+import { MotionBox, MotionStack } from '../components/animations/GlobalAnimations'
 
 export default function HomePage() {
   useEffect(() => {
@@ -24,8 +25,50 @@ export default function HomePage() {
 
       <IntroBanner />
 
-      <Flex h="120vh"></Flex>
+      <Flex 
+        h="160vh" 
+        p="4" 
+        justify="center" 
+        backgroundColor="black"
+        position="relative"
+        zIndex={2}
+      >
+        <MotionBox>
+          <Image src="/images/coin.gif" width={60} height="60" />
+          <Image src="/images/coin.gif" width={60} height="60" />
+          <Image src="/images/coin.gif" width={60} height="60" />
+        </MotionBox>
 
+        <MotionBox w="100%" textAlign="center">
+          <Text as="h1">Gerencie suas métricas</Text>
+          <Text>
+            Controle todos os seus dados em poucos minutos.
+            Use as funções especiais de nossa plataforma para análise
+          </Text>
+        </MotionBox>
+
+        <MotionStack spacing="2">
+          <Box>
+            <Text as="h3">Gerencie métricas</Text>
+            <Text>Manipule todos os seus dados de forma simples e com agilidade</Text>
+          </Box>
+
+          <Box>
+            <Text as="h3">Layout agradável</Text>
+            <Text>Experiência de uso única no uso da plataforma, com uma interface simples e agradável</Text>
+          </Box>
+
+          <Box>
+            <Text as="h3">Segurança dos dados</Text>
+            <Text>Garantia total na segurança em todas as operações realizadas na Dashgo</Text>
+          </Box>
+
+          <Box>
+            <Text as="h3">Informações em tempo real</Text>
+            <Text>Todas as operações e métricas são atualizadas simultâneamente em tempo real durante todo seu uso</Text>
+          </Box>
+        </MotionStack>
+      </Flex>
     </MotionBox>
   )
 }
