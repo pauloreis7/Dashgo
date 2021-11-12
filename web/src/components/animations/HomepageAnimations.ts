cdbe83bb8e7182e9b1e3c5fb884d89397ef8495f
Paulo Reis <paulosilvadosreis2057@gmail.com>
2021-11-12 17:02:42 -0300

    Add coins animation to InfoCoins animated component

--- web/src/components/animations/HomepageAnimations.ts
+++ web/src/components/animations/HomepageAnimations.ts
@@ -30,3 +30,18 @@ export const fadeInUpHome = {
     },
   }
 }
+
+export const coinsHome = {
+  initial: { opacity: 0, scale: 0},
+  animate: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delayChildren: 0.2,
+      delay: 0.3,
+      staggerChildren: 0.5,
+      ease: easing,
+      duration: 0.6,
+    },
+  }
+}
