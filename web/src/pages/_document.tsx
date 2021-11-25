d71e3215693fa098843bc267fdc111d8e30610d2
Paulo Reis <paulosilvadosreis2057@gmail.com>
2021-11-24 21:13:51 -0300

    Add favicon

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -7,6 +7,8 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
+
+          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
         </Head>
 
         <body>
